66ca893421eca00d90993eebb0d30ba89f2dadd7
Zahid Ali <zahidalicodes@gmail.com>
2025-10-07 17:10:16 +0500

    about page

---
+++ js/map.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+      // Initialize map
+      var map = L.map("global-map", {
+        center: [38.9072, -77.0369], // Washington, DC
+        zoom: 9,
+        zoomControl: false, // remove +/-
+      });
+
+      // Add map tiles (Google-like style)
+      L.tileLayer(
+        "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+        {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }
+      ).addTo(map);
+
+      // Add marker (no popup)
+      L.marker([38.9072, -77.0369]).addTo(map);
+    });
